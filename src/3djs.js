b5577e5ffd4cfe488b38cf3c637b9e2a70958d5b
SmartDevSource <emmanuel.beaugendre@outlook.fr>
2024-04-26 21:27:34 +0200

    files

--- src/3djs.js
+++ src/3djs.js
@@ -1,61 +1,44 @@
-import { Triangle2D, Vector2, Vector3, Eq, Triangle3D }  from "./mathlib.js"
+import { drawShape, Shape } from "./shapes.js"
 
 ///////////////// DECLARATIONS /////////////////
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 const screen = {w: window.innerWidth, h: window.innerHeight}
 
+const windowScale = window.devicePixelRatio
+canvas.width = canvas.offsetWidth * windowScale
+canvas.height = canvas.offsetHeight * windowScale
+
 ///////////////// EVENTS LISTENERS /////////////////
 window.addEventListener("resize", ()=>{
     screen.w = window.innerWidth
     screen.h = window.innerHeight
-    console.log("Screen size :", screen)
 })
 
 //////////////// SHAPES ////////////////
-const triangle3D = new Triangle3D(
-    new Vector3(-10, 10, 1),
-    new Vector3(20, 10, 1),
-    new Vector3(10, -20, 1),
-)
-///////////////// FUNCTIONS /////////////////
-let rx = 0.000
+const shape = new Shape([
+                        [200, 200, 10],
+                        [250, 200, 10],
+                        [280, 250, 10],
+                        [220, 250, 10],
+
+                        [200, 200, -10],
+                        [250, 200, -10],
+                        [280, 250, -10],
+                        [220, 250, -10],
+                    ])
+shape.scale(0)
+        
+let angle = 0
 
 const scene = () => {
-    ctx.fillStyle = "red"
-    // drawTriangle(triangle2D)
-    drawTriangle(triangle3D.translate(new Vector3(200, 50, 0)).projection())
+    angle += 0.01
     requestAnimationFrame(scene)
     ctx.clearRect(0, 0, screen.w, screen.h)
-    ctx.fillStyle = "red"
-    drawTriangle(triangle3D
-        .rotY(rx)
-        .translate(new Vector3(50, 50, 0))
-        .projection())
-    rx+=.0001
-}
-
-const drawTriangle = (triangle) => {
-    const xmin = Math.round(Math.min(triangle.v1.x, triangle.v2.x, triangle.v3.x))
-    const xmax = Math.round(Math.max(triangle.v1.x, triangle.v2.x, triangle.v3.x))
-    const ymin = Math.round(Math.min(triangle.v1.y, triangle.v2.y, triangle.v3.y))
-    const ymax = Math.round(Math.max(triangle.v1.y, triangle.v2.y, triangle.v3.y))
-
-    for (let y = ymin; y <= ymax; y++){
-        if (y >= 0 && y <= screen.h){
-            for(let x = xmin; x <= xmax; x++){
-                if (x >= 0 && x <= screen.w){
-                    const pos = new Vector2(x, y)
-                    const w1 = Eq(pos, triangle.v3, triangle.v1)
-                    const w2 = Eq(pos, triangle.v1, triangle.v2)
-                    const w3 = Eq(pos, triangle.v2, triangle.v3)
-                    if ((w1 > 0 && w2 > 0 && w3 > 0) || (-w1 > 0 && -w2 > 0 && -w3 > 0)){
-                        ctx.fillRect(pos.x, pos.y, 1, 1)
-                    }
-                }
-            }
-        }
-    }
+    // shape.rotation.x = angle
+    // shape.rotation.y = angle
+    // shape.rotation.z = angle
+    drawShape(ctx, shape, true)
 }
 
 scene()
--- src/mathlib.js
+++ src/mathlib.js
@@ -1,76 +1,52 @@
-export const Eq = (a, b, p) => (a.x-p.x)*(b.y-p.y)-(a.y-p.y)*(b.x-p.x)
-
-export class Vector2{
-    constructor(x, y){
-        this.x = x
-        this.y = y
-    }
-    innerDivider(v){
-        return new Vector2(this.x / v, this.y / v)
-    }
-}
-
+///////////////// CLASSES /////////////////
 export class Vector3{
-    constructor(x,y,z){
+    constructor(x, y, z){
         this.x = x
         this.y = y
         this.z = z
     }
-    rotX(pitch){
-        const cosPitch = Math.cos(pitch)
-        const sinPitch = Math.sin(pitch)
-        const y = cosPitch * this.y - sinPitch * this.z
-        const z = sinPitch * this.y + cosPitch * this.z
-        return new Vector3(this.x, y, z)
-    }
-    rotY(yaw){
-        const cosYaw = Math.cos(yaw)
-        const sinYaw = Math.sin(yaw)
-        const x = cosYaw * this.x + sinYaw * this.z
-        const z = -sinYaw * this.x + cosYaw * this.z
-        console.log("x", x)
-        console.log("z", z)
-        return new Vector3(x, this.y, z)
-    }
-    innerDivider(v){
-        return new Vector3(this.x / v.x, this.y / v.y, this.z / v.z)
-    }
-    innerAdder(v){
-        return new Vector3(this.x + v.x, this.y + v.y, this.z + v.z)
-    }
-    projection(){
-        return new Vector2(this.x, this.y).innerDivider(this.z)
-    }
 }
 
-export class Triangle2D{
-    constructor(v1, v2, v3){
-        this.v1 = v1
-        this.v2 = v2
-        this.v3 = v3
+///////////////// FUNCTIONS /////////////////
+export const MatMul = (a, b) => {
+    const matrix = new Array(a.length)
+    const length = matrix.length
+    for (let i = 0 ; i < length ; i++){
+        matrix[i] = new Array(length)
+        for (let j = 0 ; j < length ; j++){
+            matrix[i][j] = 0
+            for (let k = 0 ; k < length ; k++){
+                matrix[i][j] += a[i][k] * b[k][j]
+            }
+            if (isNaN(matrix[i][j])) matrix[i][j] = 0
+        }
     }
+    return matrix
 }
 
-export class Triangle3D{
-    constructor(v1, v2, v3){
-        this.v1 = v1
-        this.v2 = v2
-        this.v3 = v3
-    }
-    projection(){
-        return new Triangle2D(this.v1.projection(), this.v2.projection(), this.v3.projection())
-    }
-    translate(v){
-        return new Triangle3D(this.v1.innerAdder(v), this.v2.innerAdder(v), this.v3.innerAdder(v))
-    }
-    rotX(pitch){
-        return new Triangle3D(this.v1.rotX(pitch), 
-                              this.v2.rotX(pitch), 
-                              this.v3.rotX(pitch))
-    }
-    rotY(yaw){
-        return new Triangle3D(this.v1.rotY(yaw),
-                              this.v2.rotY(yaw),
-                              this.v3.rotY(yaw))
-    }
+export const MatToVec = (m) => {
+    return m.length == 2 ? {x: m[0][0], y: m[1][0]} :
+                           {x: m[0][0], y: m[1][0], z: m[2][0]}
+}
+
+export const Rotation = angle => {
+    return  {x: 
+                [
+                    [1, 0, 0],
+                    [0, Math.cos(angle), -Math.sin(angle)],
+                    [0, Math.sin(angle), Math.cos(angle)]
+                ],
+            y:
+                [
+                    [Math.cos(angle), 0, Math.sin(angle)],
+                    [0, 1, 0],
+                    [-Math.sin(angle), 0, Math.cos(angle)]
+                ],
+            z:
+                [
+                    [Math.cos(angle), -Math.sin(angle), 0],
+                    [Math.sin(angle), Math.cos(angle), 0],
+                    [0, 0, 1]
+                ]
+            }
 }
